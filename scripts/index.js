afb3b0f64c524c7316c5d0c65677e40a669ce07c
Vladimir Chupin <ceochupin@yandex.ru>
2024-10-29 12:28:30 +0300

    create add/delete card in page

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
-// @todo: Темплейт карточки
+// Функция для создания карточки
+function createCard(cardData, deleteCard) {
+  // Клонируем шаблон
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: DOM узлы
+  // Объявляем переменные элементов
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Функция создания карточки
+  // Устанавливаем значения элементов карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  // Добавляем обработчик клика для кнопки удаления
+  cardDeleteButton.addEventListener('click', () => deleteCard(cardElement));
 
-// @todo: Вывести карточки на страницу
+  // Возвращаем сгенерированную карточку
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Находим контейнер для карточек
+const cardsContainer = document.querySelector('.places__list');
+
+// Выводим все карточки из массива initialCards
+initialCards.forEach(cardData => {
+  const cardElement = createCard(cardData, deleteCard);
+  cardsContainer.appendChild(cardElement);
+});
